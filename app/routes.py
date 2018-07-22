a49a59f73c895f98956a432dbd4c951882ff10e2
xiwei <shuming496@outlook.com>
2018-07-22 12:52:32 +0800

    Part11 End

--- app/routes.py
+++ app/routes.py
@@ -32,9 +32,9 @@ def index():
     page = request.args.get('page', 1, type=int)
     posts = current_user.followed_posts().paginate(
         page, app.config['POSTS_PER_PAGE'], False)
-    next_url = url_for('index', page=posts.next_num) \
+    next_url = url_for('explore', page=posts.next_num) \
         if posts.has_next else None
-    prev_url = url_for('index', page=posts.prev_num) \
+    prev_url = url_for('explore', page=posts.prev_num) \
         if posts.has_prev else None
     return render_template('index.html', title='Home page', form=form, posts=posts.items,
                            next_url=next_url, prev_url=prev_url)
