d21e9af93cd9ca71acc7bd26ba358527078124ab
xiwei <shuming496@outlook.com>
2018-07-22 13:08:59 +0800

    Part12 End

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,7 @@ from logging.handlers import SMTPHandler, RotatingFileHandler
 
 from flask import Flask
 from flask_mail import Mail
+from flask_moment import Moment
 
 from config import Config
 from flask_sqlalchemy import SQLAlchemy
@@ -19,6 +20,7 @@ login = LoginManager(app)
 login.login_view = 'login'
 mail = Mail(app)
 bootstrap = Bootstrap(app)
+moment = Moment(app)
 
 if not app.debug:
     # if app.config['MAIL_SERVER']:
