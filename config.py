31e172560f69dcde3b70d93a41b59f6f56df37a9
xiwei <shuming496@outlook.com>
2018-07-26 20:23:33 +0800

    Update config.env to .env

--- config.py
+++ config.py
@@ -2,7 +2,7 @@ import os
 from dotenv import load_dotenv
 
 basedir = os.path.abspath(os.path.dirname(__file__))
-load_dotenv(os.path.join(basedir, 'config.env'))
+load_dotenv(os.path.join(basedir, '.env'))
 
 
 class Config(object):
